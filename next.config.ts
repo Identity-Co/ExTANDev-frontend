0cb406b839815253183825f7d8987e92fd6c2991
Nilesh Gadhiya <30853858+epixeltechnologies@users.noreply.github.com>
2025-08-06 11:24:05 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,23 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   basePath: process.env.BASEPATH,
   redirects: async () => {
-    return [{
-    source: '/',
-    destination: '/dashboards/crm',
-    permanent: true
-  }]}
+    return [
+      {
+        source: '/login',
+        destination: '/admin/login',
+        permanent: true
+      },
+      {
+        source: '/admin',
+        destination: '/admin/dashboards',
+        permanent: true
+      },
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true
+      }
+    ]}
 }
 
 export default nextConfig
