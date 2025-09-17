b18d8873dc066cedd2b13821a115cea91ebb0b62
Nilesh Gadhiya <30853858+epixeltechnologies@users.noreply.github.com>
2025-09-17 18:02:36 +0530

    Update page.tsx

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -6,8 +6,9 @@ import { getServerMode } from '@core/utils/serverHelpers'
 
 import { getPageBanner } from '@/app/server/banners'
 import { getPageData } from '@/app/server/home_page'
-import { getPageAdventureGuide } from '@/app/server/adventure_guide'
+import { getPageFieldNote } from '@/app/server/field_notes'
 import { getPageDestination } from '@/app/server/destinations'
+// import { getFilterLocations } from '@/app/server/tours'
 
 const LandingPage = async () => {
   // Vars
@@ -17,11 +18,13 @@ const LandingPage = async () => {
 
   const pgData = await getPageData();
 
-  const fieldnotes = await getPageAdventureGuide(pgData?.field_notes??[]);
+  const fieldnotes = await getPageFieldNote(pgData?.field_notes??[]);
 
   const destinations = await getPageDestination(pgData?.destinations??[]);
 
-    return  <LandingPageWrapper mode={mode} 
+  // const filter_locations = await getFilterLocations();
+
+  return  <LandingPageWrapper mode={mode} 
             banners={banners}
             pgData = {pgData}
             fieldNotes={fieldnotes}
