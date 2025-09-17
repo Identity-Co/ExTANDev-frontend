a99823147ecfeaba2503ea15227ff1298cd29dbc
Nilesh Gadhiya <30853858+epixeltechnologies@users.noreply.github.com>
2025-09-17 17:49:45 +0530

    style updates

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -6,9 +6,8 @@ import { getServerMode } from '@core/utils/serverHelpers'
 
 import { getPageBanner } from '@/app/server/banners'
 import { getPageData } from '@/app/server/home_page'
-import { getPageFieldNote } from '@/app/server/field_notes'
+import { getPageAdventureGuide } from '@/app/server/adventure_guide'
 import { getPageDestination } from '@/app/server/destinations'
-import { getFilterLocations } from '@/app/server/tours'
 
 const LandingPage = async () => {
   // Vars
@@ -18,18 +17,15 @@ const LandingPage = async () => {
 
   const pgData = await getPageData();
 
-  const fieldnotes = await getPageFieldNote(pgData?.field_notes??[]);
+  const fieldnotes = await getPageAdventureGuide(pgData?.field_notes??[]);
 
   const destinations = await getPageDestination(pgData?.destinations??[]);
 
-  const filter_locations = await getFilterLocations();
-
-  return  <LandingPageWrapper mode={mode} 
+    return  <LandingPageWrapper mode={mode} 
             banners={banners}
             pgData = {pgData}
             fieldNotes={fieldnotes}
             destinations={destinations}
-            filter_locations={filter_locations}
           />
 }
 
