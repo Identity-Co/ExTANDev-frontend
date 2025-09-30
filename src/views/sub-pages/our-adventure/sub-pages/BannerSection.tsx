a1747b5af90eeca36ca7387fb68913f5d1721d6f
Nilesh Gadhiya <nilesh@zooliad.com>
2025-09-30 12:21:47 +0530

    Our Adventure - updates

--- src/views/sub-pages/our-adventure/sub-pages/BannerSection.tsx
+++ src/views/sub-pages/our-adventure/sub-pages/BannerSection.tsx
@@ -105,7 +105,7 @@ const BannerSection = ({ mode, filter_categories }: { mode: Mode; filter_categor
                     <div className={classnames(styles.search_row)}>
                         <form action="/our-adventure/" method="get">
                             <div className={classnames(styles.search_select, styles.ss1)}>
-                                <label>Travel Style</label>
+                                <label>Activity</label>
                                 <select name="category" id="category" required value={selectedCategory} onChange={handleCategoryChange}>
                                   <option value="">Select a Travel Style</option>
                                   {filter_categories.map((loc) => (
