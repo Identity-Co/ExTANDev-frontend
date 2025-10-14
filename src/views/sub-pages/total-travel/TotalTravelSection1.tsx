25020db661eb94b9191ee3bae377141067306d9d
Nilesh Gadhiya <nilesh@zooliad.com>
2025-10-14 11:16:37 +0530

    Update TotalTravelSection1.tsx

--- src/views/sub-pages/total-travel/TotalTravelSection1.tsx
+++ src/views/sub-pages/total-travel/TotalTravelSection1.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 
 // Third-party Imports
 import classnames from 'classnames'
@@ -10,58 +10,98 @@ import styles from './styles.module.css'
 
 const TotalTravelSection1 = ({ data }: { data?: [] }) => {
 
-    useEffect(() => {
-        // Function to dynamically load the travel client script
-        const script = document.createElement('script')
+    const isLoadRef = useRef(false)
+    const isLoaded = useRef(false)
 
-        //script.src = 'https://booking.accessdevelopment.com/scripts/travel.client.v2.js'
-        script.src = 'https://booking-stage.accessdevelopment.com/scripts/travel.client.v2.js'
-        script.async = true
+    const fetchCarsData = async () => {
 
-        script.onload = () => {
-          console.log('Travel Client script loaded.')
+      
 
-          // Wait a bit for the library to attach itself to window
-          const checkClient = setInterval(() => {
-            if (window.travelClient) {
-              clearInterval(checkClient)
 
-              try {
-                window.travelClient.start({
-                  //session_token: "ACCESS_SESSION_s0xdt6nlUYd4ryTlmsUjNuKvw4ZVLsUF", // LIVE
-                  session_token: 'ACCESS_SESSION_itR_bDu1Lg7HGM7msqxejJa8LyKBJnJv', // STAGE
-                  container: '.hotel_search_selector',
-                  navigate_to: {
-                    view: 'cars',
-                    destination: 'MCO - Orlando International Airport - Orlando United States',
-                    return_destination: 'TPA - Tampa International Airport - Tampa United States',
-                    pickup_date_time: '2025-10-20T10:00',
-                    return_date_time: '2025-10-22T14:00',
-                  },
-                })
+      const res = await fetch(`https://auth.adcrws-stage.com/api/v1/tokens`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          "member_key" : "AN202510140004",
+          "email"      : "access004@gmail.com",
+          "first_name" : "John",
+          "last_name"  : "Two",
+          "scope"      : "travel"
+        })
+      });
 
-                window.travelClient.on('error', function (err) {
-                  console.error('Travel Client error:', err)
-                })
-              } catch (err) {
-                console.error('Error initializing travelClient:', err)
-              }
+      //const res = {"session_token":"ACCESS_SESSION_YXixlZT-e7tzylrhUfOVnCRFUfOFWcWO"};
+
+      //const log = await res.json()
+
+      console.log(' New Login :: ', res)
+
+      /*if (res && res.ok) {
+
+      // Function to dynamically load the travel client script
+      const script = document.createElement('script')
+
+      //script.src = 'https://booking.accessdevelopment.com/scripts/travel.client.v2.js'
+      script.src = 'https://booking-stage.accessdevelopment.com/scripts/travel.client.v2.js'
+      script.async = true
+
+      script.onload = () => {
+        console.log('Travel Client script loaded.')
+
+        // Wait a bit for the library to attach itself to window
+        const checkClient = setInterval(() => {
+          if (window.travelClient) {
+            clearInterval(checkClient)
+
+            try {
+              window.travelClient.start({
+                //session_token: "ACCESS_SESSION_s0xdt6nlUYd4ryTlmsUjNuKvw4ZVLsUF", // LIVE
+                session_token: 'ACCESS_SESSION_itR_bDu1Lg7HGM7msqxejJa8LyKBJnJv', // STAGE
+                container: '.hotel_search_selector',
+                navigate_to: {
+                  view: 'cars',
+                  destination: 'MCO - Orlando International Airport - Orlando United States',
+                  return_destination: 'TPA - Tampa International Airport - Tampa United States',
+                  pickup_date_time: '2025-10-20T10:00',
+                  return_date_time: '2025-10-22T14:00',
+                },
+              })
+
+              window.travelClient.on('error', function (err) {
+                console.error('Travel Client error:', err)
+              })
+            } catch (err) {
+              console.error('Error initializing travelClient:', err)
             }
-          }, 500)
-        }
+          }
+        }, 500)
+      }
 
-        script.onerror = () => {
-          console.error('Failed to load Travel Client script.')
-        }
+      script.onerror = () => {
+        console.error('Failed to load Travel Client script.')
+      }
 
-        document.body.appendChild(script)
+      document.body.appendChild(script)
 
-        // Cleanup on unmount
-        return () => {
-          if (script.parentNode) script.parentNode.removeChild(script)
-        }
+      // Cleanup on unmount
+      return () => {
+        if (script.parentNode) script.parentNode.removeChild(script)
+      }*/
+
+      isLoaded.current = true
+
+    };
+
+    useEffect(() => {
+        if (isLoadRef.current) return;
+        isLoadRef.current = true
+
+        fetchCarsData();
+
     }, [])
-  
+
   return (
     <section className={classnames(styles.home_section1, styles.our_desti_sec1)}>
         <div className="container">
