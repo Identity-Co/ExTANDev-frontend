34bbad6d839c3194d476a1c36868e404b6915738
Nilesh <30853858+epixeltechnologies@users.noreply.github.com>
2025-11-25 15:13:29 +0530

    All Adventures + Total travel + Menu Updates

---
+++ src/views/sub-pages/all-adventures/BannerSection.tsx
@@ -0,0 +1,277 @@
+// React Imports
+import { useEffect, useRef, useState } from 'react'
+import { useSearchParams } from 'next/navigation'
+
+// Third-party Imports
+import Slider from 'react-slick';
+import classnames from 'classnames'
+
+// Type Imports
+import type { Mode } from '@core/types'
+
+import { getDestinationsByCustomCategory } from '@/app/server/tours'
+
+// Styles Imports
+import styles from './styles.module.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const BannerSection = ({ mode, banners, filter_categories, scrollRef }: { mode: Mode; banners?: []; filter_categories: []; scrollRef?: []; }) => {
+  const slideref = useRef();
+  const [destinations, setDestinations] = useState<string[]>([])
+  const [selectedCategory, setSelectedCategory] = useState('')
+  const [selectedDestination, setSelectedDestination] = useState('')
+
+  const [openAct, setOpenAct] = useState(false);
+  const [selectedAct, setSelectedAct] = useState("Choose an Activity");
+
+  const [openDest, setOpenDest] = useState(false);
+  const [selectedDest, setSelectedDest] = useState("Select a Destination");
+
+  const searchParams = useSearchParams()
+  
+  useEffect(() => {
+    const slides = document.querySelectorAll('.hero_slide_box');
+
+    slides.forEach((slide) => {
+      const backgroundImage = slide.style.backgroundImage;
+
+      if (!backgroundImage || backgroundImage != slide.getAttribute('databackground')) {
+        slide.style.backgroundImage = 'url("'+slide.getAttribute('databackground')+'")';
+      }
+    });
+
+    const loadDestinations = async (categoryFromUrl) => {
+      const matchedCategory = filter_categories.find(loc => loc.category_name === categoryFromUrl)
+      if (matchedCategory) {
+        const cat_id = matchedCategory._id
+        const data = await getDestinationsByCustomCategory(cat_id)
+        setDestinations(data || [])
+      }
+    }
+
+    //const searchParams = new URLSearchParams(window.location.search)
+    const categoryFromUrl = searchParams.get('category')
+    const destinationFromUrl = searchParams.get('destination')
+
+    if (categoryFromUrl) {
+      setSelectedAct(categoryFromUrl);
+      setSelectedCategory(categoryFromUrl)
+      loadDestinations(categoryFromUrl);
+    } else {
+      setSelectedCategory('')
+      setDestinations([])
+    }
+
+    if (destinationFromUrl) {
+      setSelectedDestination(destinationFromUrl)
+    } else {
+      setSelectedDestination('')
+    }
+  }, [searchParams]);
+
+  const settings = {
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 1000,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  // Handle Category change
+  // const handleCategoryChange = async (e: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleCategoryChange = async (category) => {
+    // const selectedOption = e.target.selectedOptions[0]
+
+    /*const categoryName = e.target.value
+    const category = selectedOption.getAttribute("data-id");
+    setSelectedCategory(categoryName)*/
+    setSelectedDestination('')
+    setDestinations([])
+
+    if (category) {
+      try {
+        const data = await getDestinationsByCustomCategory(category)
+        setDestinations(data || [])
+      } catch (err) {
+        console.error('Failed to fetch destinations:', err)
+        setDestinations([])
+      }
+    }
+  }
+
+  const icons = ["bicycling-svgrepo-com.svg", "cruising.svg", "culture-immersion.svg", "walking-svgrepo-com.svg", "rail.svg", "skiing.svg", "surf.svg"]
+
+  return (
+    <div className={classnames(styles.home_banner, styles.destination_overview_banner, styles.adventure_banner, 'home_banner top-banner destination_overview_banner')}>
+        <div className={classnames(styles.home_banner_slide)}>
+            <Slider {...settings} className={classnames(styles.home_hero_slider, 'home_hero_slider')}>
+              {banners?.map((item, index) => {
+                const imageUrl = `${process.env.NEXT_PUBLIC_UPLOAD_URL}/\\${item.banner_image}`;
+
+                return (
+                  <div 
+                    key={`adv-slider-${index}`}
+                    className={classnames(styles.hero_slide_box, 'hero_slide_box')} 
+                    ref={slideref} databackground={imageUrl} 
+                    style={{
+                      backgroundImage: `url("${imageUrl}")`,
+                    }}
+                  >
+                      <div className={classnames(styles.hero_slide_container)}>
+                          <div className={classnames(styles.hero_slide_text)}>
+                              {item.title && <h1>{item.title}</h1>}
+                              {item.sub_title && <p>{item.sub_title}</p>}
+                          </div>
+
+                          {item.location && (
+                            <div className={classnames(styles.hero_slide_location)}>
+                              <div className={classnames(styles.location_name)}>
+                                <img src="/images/front-pages/images/hero-location.svg" />
+                                <span>{item.location}</span>
+                              </div>
+                            </div>
+                          )}
+                      </div>
+                  </div>
+                );
+              })}
+            </Slider>
+        </div>
+        <div className={classnames(styles.search_box)} ref={scrollRef}>
+            <div className={classnames(styles.container, 'container')}>
+                <div className={classnames(styles.search_box_inner)}>
+                    <div className={classnames(styles.search_row)}>
+                        <form action="/our-adventure/" method="get">
+                            <input type="hidden" name="category" value={selectedCategory??''} />
+                            <input type="hidden" name="destination" value={selectedDestination??''} />
+
+                            <div className={classnames(styles.search_select, styles.ss1)}>
+                                <label>Activity</label>
+                                {/*<select name="category" id="category" required value={selectedCategory} onChange={handleCategoryChange}>
+                                  <option value="">Choose an Activity</option>
+                                  {filter_categories.map((loc) => (
+                                    <option key={loc._id} value={loc.category_name} data-id={loc._id}>
+                                      {loc.category_name}
+                                    </option>
+                                  ))}
+                                </select>*/}
+
+                                <div className={`custom-select ${openAct ? 'active' : ''}`}>
+                                  <div 
+                                    className="select-selected"
+                                    onClick={() => {setOpenAct(!openAct); setOpenDest(false)}}
+                                    style={{ cursor: "pointer" }}
+                                  >
+                                    <div>
+                                      <span className="select-icn">
+                                        <img src="/images/svg/walking-svgrepo-com.svg" alt=""  />
+                                      </span>
+                                      <span>{selectedAct}</span>
+                                    </div>
+
+                                    <img
+                                      src="/images/svg/down-arrow.svg"
+                                      alt=""
+                                      style={{
+                                        transform: openAct ? "rotate(180deg)" : "rotate(0deg)",
+                                        transition: "0.2s",
+                                      }}
+                                    />
+                                  </div>
+
+                                  {openAct && (
+                                    <div className="select-items">
+                                      {filter_categories.map((loc, index) => (
+                                        <div
+                                          key={index}
+                                          onClick={() => {
+                                            setSelectedAct(loc.category_name);
+                                            setSelectedCategory(loc.category_name)
+                                            setOpenAct(false);
+                                            handleCategoryChange(loc._id)
+                                          }}
+                                          style={{ cursor: "pointer" }}
+                                        >
+                                          
+                                          <span>
+                                            { icons[index] ? (<img src={`/images/svg/${icons[index]}`} alt="" />) : (<img src="/images/svg/walking-svgrepo-com.svg" alt="" />) }
+                                          </span>
+                                          {loc.category_name}
+                                        </div>
+                                      ))}
+                                    </div>
+                                  )}
+                                </div>
+                            </div>
+                            <div className={classnames(styles.search_select, styles.ss2)}>
+                                <label>Destinations</label>
+                                {/*<select name="destination" id="destination" value={selectedDestination} onChange={(e) => setSelectedDestination(e.target.value)}>
+                                  <option value="">Select a Destination</option>
+                                  {Array.isArray(destinations) && destinations.map((dest: string) => (
+                                    <option key={dest} value={dest}>
+                                      {dest}
+                                    </option>
+                                  ))}
+                                </select>*/}
+                                <div className={`custom-select ${openDest ? 'active' : ''}`}>
+                                  <div 
+                                    className="select-selected"
+                                    onClick={() => {setOpenDest(!openDest); setOpenAct(false)}}
+                                    style={{ cursor: "pointer" }}
+                                  >
+                                    <div>
+                                      <span className="select-icn">
+                                        <img src="/images/svg/map-pin.svg" alt=""  />
+                                      </span>
+                                      <span>{selectedDest}</span>
+                                    </div>
+
+                                    <img
+                                      src="/images/svg/down-arrow.svg"
+                                      alt=""
+                                      style={{
+                                        transform: openDest ? "rotate(180deg)" : "rotate(0deg)",
+                                        transition: "0.2s",
+                                      }}
+                                    />
+                                  </div>
+
+                                  {openDest && (
+                                    <div className="select-items">
+                                      {Array.isArray(destinations) && destinations.map((dest: string) => (
+                                        <div
+                                          key={dest}
+                                          onClick={() => {
+                                            setSelectedDest(dest);
+                                            setSelectedDestination(dest)
+                                            setOpenDest(false);
+                                          }}
+                                          style={{ cursor: "pointer" }}
+                                        >
+                                          <span>
+                                            <img src="/images/svg/map-pin.svg" alt="" />
+                                          </span>
+                                          {dest}
+                                        </div>
+                                      ))}
+                                    </div>
+                                  )}
+                                </div>
+                            </div>
+                            <div className={classnames(styles.search_btn)}>
+                                <input type="submit" name="" value="Search" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default BannerSection
---
+++ src/views/sub-pages/all-adventures/OurAdventureSection2.tsx
@@ -0,0 +1,139 @@
+// React Imports
+import { useEffect, useState } from 'react'
+
+//import { getFilteredTours } from '@/app/server/tours'
+import { getAllToursForSEO } from '@/app/server/tours'
+
+// Third-party Imports
+import classnames from 'classnames'
+
+// Styles Imports
+import styles from './styles.module.css'
+
+const OurAdventureSection2 = ({ data, toursData }: { data?: []; toursData?: []; }) => {
+  const [selectedCategory, setSelectedCategory] = useState('')
+  const [selectedDestination, setSelectedDestination] = useState('')
+  const [currentTours, setCurrentTours] = useState(toursData)
+  const [currentPage, setCurrentPage] = useState(1)
+  const recordsPerPage = 100000
+  const totalPages = 1;
+
+  const fetchTours = async (category: number, destination: string, page: number) => {
+    //const data = await getFilteredTours(category, destination, page)
+    const data = await getAllToursForSEO(category, destination, page)
+    setCurrentTours(data)
+  }
+
+  // Update currentPage from URL query if exists
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search)
+    const page = parseInt(params.get('page') || '1', 10)
+    const clampedPage = Math.min(Math.max(page, 1), totalPages)
+
+    setCurrentPage(clampedPage)
+
+    const categoryFromUrl = params.get('category') || ''
+    const destinationFromUrl = params.get('destination') || ''
+    setSelectedCategory(categoryFromUrl)
+    setSelectedDestination(destinationFromUrl)
+
+    const loadTours = async () => {
+      await fetchTours(categoryFromUrl, destinationFromUrl, clampedPage)
+    }
+
+    //fetchTours(categoryFromUrl, destinationFromUrl, page)
+    loadTours()
+  }, [totalPages])
+
+    // Handle page change
+    const handlePageChange = (page: number) => {
+      const clampedPage = Math.min(Math.max(page, 1), totalPages)
+      setCurrentPage(clampedPage)
+
+      const params = new URLSearchParams(window.location.search)
+      params.set('page', clampedPage.toString())
+      window.history.pushState({}, '', `${window.location.pathname}?${params.toString()}`)
+
+      fetchTours(selectedCategory, selectedDestination, clampedPage)
+    }
+
+    const getPaginationRange = (
+      currentPage: number,
+      totalPages: number,
+      delta = 3
+    ): (number | string)[] => {
+      if (totalPages <= 1) return []
+
+      const range: (number | string)[] = []
+      const left = Math.max(currentPage - delta, 2)
+      const right = Math.min(currentPage + delta, totalPages - 1)
+
+      range.push(1)
+      if (left > 2) range.push('...')
+      for (let i = left; i <= right; i++) range.push(i)
+      if (right < totalPages - 1) range.push('...')
+      range.push(totalPages)
+
+      return range
+    }
+
+  return (
+    <section className={classnames(styles.our_desti_sec2, styles.our_adven_sec2)}>
+        <div className="container">
+            <div className="head_text_center">
+                {data?.feature_adventure_title && (<h2 className={classnames("fs_55")}>{data?.feature_adventure_title}</h2>)}
+            </div>
+            <div className={classnames(styles.feature_desti_row, styles.feature_adven_row)}>
+
+                {currentTours.map((tour) => {
+                    const price = tour.departures?.[0]?.lowest_price ?? "N/A";
+                    const startDate = tour.departures?.[0]?.start_date
+                      ? new Date(tour.departures[0].start_date).toLocaleDateString()
+                      : "N/A";
+                    return (
+                        <div className={classnames(styles.feature_adven_box)} key={tour._id}>
+                            <div className={classnames(styles.feature_adven_top)}>
+                                <div className={classnames(styles.feature_adven_img)}>
+                                    <img className="bx_sd" src={tour.image} />
+                                </div>
+                            </div>
+                            <div className={classnames(styles.feature_adven_bot)}>
+                                <h3>{tour.name}</h3>
+                                <span className={classnames(styles.days_price)}><span className="extra_bold">{tour.tour_days}</span> days   |  from <span className="extra_bold">USD {tour.start_price}</span>/pp <span className={classnames(styles.taxes)}>(price includes taxes and fees)</span></span>
+                                <p>{tour.description
+                                    .split(' ')
+                                    .slice(0, 50)
+                                    .join(' ')}
+                                  {tour.description.split(' ').length > 50 ? '...' : ''}</p>
+                                <div className={classnames(styles.btn, 'btn')}>
+                                    <a href={`/our-adventure/${tour.slug}/`} tabIndex="0">View Trip</a>
+                                </div>
+                            </div>
+                        </div>
+                    );
+                })}
+
+                {/* Pagination */}
+                
+            </div>
+            {/* <div className={styles.pagination}>
+              {getPaginationRange(currentPage, totalPages).map((page, idx) =>
+                typeof page === 'string' ? (
+                  <span key={idx} className={styles.ellipsis}>{page}</span>
+                ) : (
+                  <button
+                    key={idx}
+                    className={classnames({ [styles.active]: page === currentPage })}
+                    onClick={() => handlePageChange(page)}
+                  >
+                    {page}
+                  </button>
+                )
+              )}
+            </div> */}
+        </div>
+    </section>
+  )
+}
+
+export default OurAdventureSection2
